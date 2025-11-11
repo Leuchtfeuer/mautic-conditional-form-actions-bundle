b986d465973f8bf1f9d92fa216b699d136813e35
Patryk Gruszka <p.gruszka@leuchtfeuer.com>
2025-11-11 14:13:01 +0100

    [MTC-9184] config desc

--- Config/config.php
+++ Config/config.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 return [
     'name'        => 'ConditionalFormActions by Leuchtfeuer',
-    'description' => 'Import Segments directly from external sources (API, CSV, ...), or fetch properties based on query filters instead of all contacts',
+    'description' => 'Enables conditional form submit actions',
     'version'     => '0.0.1',
     'author'      => 'Leuchtfeuer Digital Marketing GmbH',
     'routes'      => [
