55115630ee8c53c750bd149d3957556bfdcf0702
Patryk Gruszka <p.gruszka@leuchtfeuer.com>
2025-11-12 12:36:16 +0100

    [MTC-9184] inject action template

---
+++ EventListener/InjectActionTemplateSubscriber.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MauticPlugin\LeuchtfeuerConditionalFormActionsBundle\EventListener;
+
+
+use Mautic\FormBundle\Event\FormBuilderEvent;
+use Mautic\FormBundle\FormEvents;
+use MauticPlugin\LeuchtfeuerDoiBundle\Integration\Config;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class InjectActionTemplateSubscriber implements EventSubscriberInterface
+{
+
+    private const ACTION_TEMPLATE = '@LeuchtfeuerConditionalFormActions/Action/_generic.html.twig';
+
+    public function __construct(
+        private Config $pluginConfig,
+    ) {
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            // execute last when all form action settings are populated
+            FormEvents::FORM_ON_BUILD => ['onFormBuild', -100],
+        ];
+    }
+
+    /**
+     * We want to set the template for all events;
+     * since there is no public method in the event, we have to make it using the ReflectionClass
+     */
+    public function onFormBuild(FormBuilderEvent $event): void
+    {
+        if (!$this->pluginConfig->isPublished()) {
+            return;
+        }
+
+        // Reflect into the private "actions" property to modify in-place.
+        $rc = new \ReflectionClass($event);
+        if (!$rc->hasProperty('actions')) {
+            return; // property missing or changed in future versions
+        }
+
+        $prop = $rc->getProperty('actions');
+        $prop->setAccessible(true);
+
+        /** @var array<string,array> $actions */
+        $actions = $prop->getValue($event);
+
+        foreach ($actions as &$action) {
+            if (empty($action['template'])) {
+                $action['template'] = self::ACTION_TEMPLATE;
+            }
+        }
+        unset($action);
+
+        $prop->setValue($event, $actions);
+    }
+
+}
