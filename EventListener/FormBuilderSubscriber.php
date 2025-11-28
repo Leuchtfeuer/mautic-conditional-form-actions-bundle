5b699f533ebe82f56bfb8297ce3bbbc37c46db84
Patryk Gruszka <p.gruszka@leuchtfeuer.com>
2025-11-28 10:01:29 +0100

    [MTC-9294] fix action with conditions remove

--- EventListener/FormBuilderSubscriber.php
+++ EventListener/FormBuilderSubscriber.php
@@ -2,8 +2,10 @@
 
 namespace MauticPlugin\LeuchtfeuerConditionalFormActionsBundle\EventListener;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Mautic\FormBundle\Event\FormEvent;
 use Mautic\FormBundle\FormEvents;
+use MauticPlugin\LeuchtfeuerConditionalFormActionsBundle\Entity\FormActionCondition;
 use MauticPlugin\LeuchtfeuerConditionalFormActionsBundle\Integration\Config;
 use MauticPlugin\LeuchtfeuerConditionalFormActionsBundle\Model\ActionConditionManager;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
@@ -14,15 +16,29 @@ class FormBuilderSubscriber implements EventSubscriberInterface
     public function __construct(
         private ActionConditionManager $actionConditionManager,
         private RequestStack $requestStack,
-        private Config $pluginConfig
+        private Config $pluginConfig,
+        private EntityManagerInterface $em
     ) {
     }
 
     public static function getSubscribedEvents(): array
     {
         return [
+            FormEvents::FORM_PRE_SAVE  => ['onFormPreSave', 0],
             FormEvents::FORM_POST_SAVE => ['onFormPostSave', 0],
         ];
+    }
+
+    public function onFormPreSave(FormEvent $event): void
+    {
+        if (!$this->pluginConfig->isPublished()) {
+            return;
+        }
+
+        // Detach any loaded FormActionCondition entities to avoid UoW processing them
+        // after actions were deleted in the controller's deleteActions() flush.
+        // @phpstan-ignore-next-line
+        $this->em->clear(FormActionCondition::class);
     }
 
     public function onFormPostSave(FormEvent $event): void
