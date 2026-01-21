4a11f75977ec0c819d5c624f0fd05cb003d1532d
Patryk Gruszka <p.gruszka@leuchtfeuer.com>
2026-01-21 10:47:25 +0100

    MTC-9665 plugin install/update M6

--- LeuchtfeuerConditionalFormActionsBundle.php
+++ LeuchtfeuerConditionalFormActionsBundle.php
@@ -10,13 +10,4 @@ use Mautic\PluginBundle\Entity\Plugin;
 
 class LeuchtfeuerConditionalFormActionsBundle extends AbstractPluginBundle
 {
-    /**
-     * @param array<int, mixed>|null $metadata
-     * @param bool|null              $installedSchema
-     */
-    public static function onPluginInstall(Plugin $plugin, MauticFactory $factory, $metadata = null, $installedSchema = null): void
-    {
-        // run DB migrations instead of automatic schema update
-        self::onPluginUpdate($plugin, $factory, $metadata);
-    }
 }
